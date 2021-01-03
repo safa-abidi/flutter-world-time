fb4705f85c6b94d86a2a8a8023cccb0e2245017a
Safa Abidi <61377020+safa-abidi@users.noreply.github.com>
2021-01-03 21:42:08 +0100

    added a spinner in loading page and day and night backgroud images

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -15,7 +16,8 @@ class _LoadingState extends State<Loading> {
     Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location': instance.location,
       'flag': instance.flag,
-      'time': instance.time
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
     });
   }
 
@@ -28,9 +30,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 80.0,
+        ),
       ),
     );
   }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime{
   String location; //location for the UI
   String time; //time in that location
   String flag; //url for an asset flag icon
   String url; //location url for api endpoint
+  bool isDaytime; //true or false if daytime or not
 
   WorldTime({this.location, this.flag, this.url});
 
@@ -24,10 +26,12 @@ class WorldTime{
       now = now.add(Duration(hours: int.parse(offset)));
 
       //set the time property (of the class)
-      time = now.toString();
+      isDaytime = now.hour > 6 && now.hour > 20 ? true : false;
+      time = DateFormat.jm().format(now);
+
     } catch(e){
       print('fama ghalta : $e');
-      time = 'could not get time data';
+      time = 'error';
     }
 
 
